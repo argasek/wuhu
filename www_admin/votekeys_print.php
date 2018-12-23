f425b9d527c03b11b06b0e48941f101306f00721
Jakub Argasiński <jakub@argasinski.eu>
2018-12-23 19:24:34 +0100

    New Feature: reserved votekeys (uses reserved_votekeys as count in Settings).

--- www_admin/votekeys_print.php
+++ www_admin/votekeys_print.php
@@ -1,50 +1,67 @@
-<?
+<?php
 include_once("bootstrap.inc.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
- <title></title>
- <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2" />
-<style type="text/css">
-body {
-  font-family: arial;
-  margin: 0px;
-  padding: 0px;
-}
+  <title></title>
+  <meta charset="utf-8">
+  <style type="text/css">
+    body {
+      font-family: arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
 
-ul {
-  margin: 0px;
-  padding: 0px;
-}
-li {
-  list-style: none;
-  padding: 15px;
-  padding-top: 25px;
-  padding-bottom: 25px;
-  border: 1px dotted #ccc;
-  text-align:center;
-  font-size: 130%;
-  letter-spacing: 2px;
-}
-.votekeys li {
-  float: left;
-  width: 25%;
-}
-<?=($settings["votekeys_css"] ?: "")?>
-</style>
+    ul {
+      margin: 0;
+      padding: 0;
+    }
+
+    li {
+      list-style: none;
+      padding: 25px 15px;
+      border: 1px dotted #ccc;
+      text-align: center;
+      font-size: 130%;
+      letter-spacing: 2px;
+    }
+
+    .votekeys li {
+      float: left;
+      width: 25%;
+    }
+
+    .votekey-reserved {
+      font-family: "Courier New", sans-serif;
+    }
+
+    .votekey-reserved:before, .votekey-reserved:after {
+      content: '*';
+    }
+
+    <?=($settings["votekeys_css"] ?: "")?>
+  </style>
 </head>
 <body>
-<?
-printf("<ul class='votekeys'>");
-$n = 1;
+<?php
 $s = SQLLib::selectRows("select * from votekeys");
-$format = $settings["votekeys_format"] ?: "{%VOTEKEY%}";
-foreach($s as $t) {
-  printf("  <li>%s</li>",str_replace("{%VOTEKEY%}",$t->votekey,$format));
+if (count($s) > 0) {
+  printf("<ul class='votekeys'>");
+  $n = 1;
+  $format = $settings["votekeys_format"] ?: "{%VOTEKEY%}";
+  foreach ($s as $t) {
+    $reserved = '';
+    if (isset($settings["reserved_votekeys"])) {
+      $reserved = ($n <= $settings["reserved_votekeys"]) ? "class='votekey-reserved'" : '';
+    }
+    printf("  <li %s>%s</li>", $reserved, str_replace("{%VOTEKEY%}", $t->votekey, $format));
+    $n++;
+  }
+  printf("</ul>");
+} else {
+  printf("<p>No votekeys generated yet!</p>");
 }
-printf("</ul>");
 
 ?>
 </body>
