c91b359d254eb8616b04db55e1dbce9485cef732
Jakub Argasiński <jakub@argasinski.eu>
2019-01-28 19:20:03 +0100

    Do not put container around news if no news found

--- www_party/include_news.php
+++ www_party/include_news.php
@@ -1,14 +1,20 @@
 <?
 if (!defined("ADMIN_DIR")) exit();
 ?>
-<dl class="text-container">
+
 <?
 $news = SQLLib::selectRows("select * from intranet_news order by `date` desc");
+if (count($news) > 0) {
+?><dl class="text-container"><?
+}
 foreach($news as $n) {
 ?>
 <dt><h4><?=date("Y-m-d",strtotime($n->date))?> - <?=_html($n->eng_title)?></h4></dt>
 <dd><?=$n->eng_body?></dd>
 <?
 }
+
+if (count($news) > 0) {
+  ?></dl><?
+}
 ?>
-</dl>
